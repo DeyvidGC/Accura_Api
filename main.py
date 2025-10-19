a1a0d3d37753d1bf5ad91de1de06cd1a9d1805bf
Deyvid Joseph Guevara Cueva <deyvid.guevara@inventiva-solutions.com>
2025-10-19 18:12:03 -0500

    feat: Update API

--- main.py
+++ main.py
@@ -1,3 +1,12 @@
-from app.main import app
+from fastapi import FastAPI
 
-__all__ = ["app"]
+from app.interfaces.api.routes import register_routes
+
+
+def create_app() -> FastAPI:
+    app = FastAPI()
+    register_routes(app)
+    return app
+
+
+app = create_app()
