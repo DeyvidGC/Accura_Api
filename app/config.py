0a9f1104f06c5ef9232a38658a9b4386764b1be6
Deyvid Joseph Guevara Cueva <deyvid.guevara@inventiva-solutions.com>
2025-10-21 00:04:11 -0500

    feat: Envio de correos exitoso

--- app/config.py
+++ app/config.py
@@ -26,7 +26,7 @@ class Settings(BaseSettings):
         default=60, description="Number of minutes before access tokens expire"
     )
     sendgrid_api_key: str | None = Field(
-        default="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",
+        default=None,
         description="SendGrid API key used for sending transactional emails via the REST API",
     )
     sendgrid_sender: str | None = Field(
