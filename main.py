47cbbe9405ba3d861eb5fcc6f38df48cce8a1985
Deyvid Joseph Guevara Cueva <deyvid.guevara@inventiva-solutions.com>
2025-10-27 20:14:25 -0500

    feat: Update peticiones

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
-from contextlib import asynccontextmanager
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+from contextlib import asynccontextmanager
 
 from app.interfaces.api.routes import register_routes
 from app.infrastructure.database import initialize_database, engine
+
 
 @asynccontextmanager
 async def lifespan(app: FastAPI):
@@ -12,9 +14,21 @@ async def lifespan(app: FastAPI):
     # 👉 libera conexiones
     engine.dispose()
 
+
 def create_app() -> FastAPI:
     app = FastAPI(lifespan=lifespan)
+
+    # Configurar CORS para permitir peticiones desde localhost:4200
+    app.add_middleware(
+        CORSMiddleware,
+        allow_origins=["http://localhost:4200"],  # Permitir solicitudes desde localhost:4200
+        allow_credentials=True,
+        allow_methods=["*"],  # Permitir todos los métodos HTTP
+        allow_headers=["*"],  # Permitir todos los encabezados
+    )
+
     register_routes(app)
     return app
+
 
 app = create_app()
